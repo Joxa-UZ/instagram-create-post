ae574325e07f1bae5e6325314fdd3b11de407694
Joxa-UZ <javokhirrustamovuz@gmail.com>
2022-10-11 11:08:39 +0500

    done bugs

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let sumbitBtn = document.querySelector(".hero__button");
+let modalBtn = document.querySelector(".modal");
+let modalPost2 = document.querySelector(".modal2");
+let cancelBtn = document.querySelector(".modal-box__cancel");
+let modalFileBtn = document.querySelector(".modal-box__file");
+let modalBackBtn = document.querySelector(".modal-box__back");
+
+
+
+sumbitBtn.addEventListener("click", function (){
+    modalBtn.classList.add("modal-active");
+})
+cancelBtn.addEventListener("click", function (){
+    modalBtn.classList.remove("modal-active");
+})
+modalFileBtn.addEventListener("click", function(){
+    modalPost2.classList.add("modal2-active");
+})
+modalBackBtn.addEventListener("click", function(){
+    modalPost2.classList.remove("modal2-active");
+})
